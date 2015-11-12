75e2abbe323b4d55f5d3d05c4a6ef227fed1d560
Victor Tang <victortang1992@gmail.com>
2015-11-11 22:32:19 -0800

    add xylo

--- app.js
+++ app.js
@@ -2,28 +2,15 @@
     //start of function
   var app = angular.module('Simon', []);
 
-app.factory('memory', function($http){
-
-  var baseUrl = "http://www.freecodecamp.com/news/hot/";
+app.factory('memory', function(){
 
   var storage = {};
  storage.datadata = [];
 
-    $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
-    $http.jsonp(baseUrl + "?callback=JSON_CALLBACK").success(function(data1) {
-
-        storage.datadata = data1;
-
-     }).error(function(data1) {
-        storage.datadata = [];
-        console.log("error0");
-     });
-    }//end info pulling
-
   return storage;
 });//end of service
 
-app.controller('MainCtrl', ['$scope', 'memory', function($scope, memory){
+app.controller('MainCtrl', ['$scope', function($scope){
     $scope.storage = memory; // load service
 
 }]);//end of controller
