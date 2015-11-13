438bf65e7db7ee4fea19e39aa2c533bf8db68efa
Victor Tang <victortang1992@gmail.com>
2015-11-12 20:59:33 -0800

    right side buttons, with lights. left side fix if not 15. backup wood texture

--- app.js
+++ app.js
@@ -14,11 +14,38 @@ app.controller('MainCtrl', ['$scope', 'memory', function($scope, memory){
     $scope.set_styling = function (input) {
         return { "height": input.length, "box-shadow": input.color }
     }
-    $scope.on = false;
+    $scope.power = false;
+    $scope.playing = false;
+    $scope.strict = false;
     $scope.showingDemo = false;
-    
-    
-    
+    $scope.count = 0;
+    $scope.OnOff = function () {
+        if ($scope.power == false){
+            $scope.power = true;
+            console.log("On.");
+        }
+        else {
+            $scope.power = false;
+        }
+    }
+    $scope.Play = function () {
+        if ($scope.playing == false){
+            $scope.playing = true;
+            console.log("On.");
+        }
+        else {
+            $scope.playing = false;
+        }
+    }
+    $scope.Snape = function () {
+        if ($scope.strict == false){
+            $scope.strict = true;
+            console.log("On.");
+        }
+        else {
+            $scope.strict = false;
+        }
+    }
     
     
     
